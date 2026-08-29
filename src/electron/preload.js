dc7874d6ad74cd5b53c296bec94070219d0e844d
mubatomobiliario-cloud <mubato.mobiliario@gmail.com>
2026-08-29 16:59:49 -0500

    Exponer ejecución editorial completa a Electron

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -5,26 +5,28 @@ console.log("Preload cargado.");
 contextBridge.exposeInMainWorld("companion", {
 
     seleccionarProyecto() {
-
         return ipcRenderer.invoke("seleccionarProyecto");
-
     },
 
     importarProyecto(carpeta) {
-
         return ipcRenderer.invoke(
             "importarProyecto",
             carpeta
         );
-
     },
 
-    analizarProyecto(carpeta) {
-
+    ejecutarProyecto(carpeta) {
         return ipcRenderer.invoke(
-            "analizarProyecto",
+            "ejecutarProyecto",
             carpeta
         );
+    },
 
+    mostrarSalidaEditorial(rutaSalida) {
+        return ipcRenderer.invoke(
+            "mostrarSalidaEditorial",
+            rutaSalida
+        );
     }
+
 });
